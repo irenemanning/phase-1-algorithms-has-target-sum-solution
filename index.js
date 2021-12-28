0a95bee933a6741e58da56639904a1817f846498
Irene <irenemanningm@gmail.com>
2021-12-28 03:23:33 -0600

    commit tree 40d0ab24f4d5d007a0ec15986a82ca11bfc0845f

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
+/*
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i]
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true
+    }
+  } 
+  return false
 }
+runtime:0(n^2)
+space: 0(n)
+*/
+//OPTOMIZED VERSION
+
+function hasTargetSum(array, target) {
+  const seenNumbers = {}
+  for (const number of array) {
+    const complement = target - number
+    if (complement in seenNumbers) return true
+    seenNumbers[number] = true
+  }
+  return false
+}
+
+
+
 
 /* 
   Write the Big O time complexity of your function here
+  Runtime:
 */
 
 /* 
-  Add your pseudocode here
+  create an object to keep track of numbers we've already seen
+  iterate through each number in array
+  for current num, identify a compliment that adds to the target (comp =  target- num)
+  iterate through rest of array
+  check if any key on our object is a compliment
+   if so return true, otherwise add to object
+  if so, return true
+  if by the end of array there is none, return false
 */
 
 /*
